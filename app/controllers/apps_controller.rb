8f62f21c646f0660f69c1cfe7a5a69b5eb680ef3
Jasper Kennis <jasper@creativetoken.nl>
2011-11-27 21:07:55 +0100

    Created "new" view, template and partial for apps.

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,4 +1,13 @@
 class AppsController < ApplicationController
 	def index
 	end
+	
+	def new
+		@app = App.new
+		
+		respond_to do |format|
+			format.html # new.html.erb
+			format.json { render :json => @post }
+		end
+	end
 end
