c2e9eb5d91cd84442510b6f37b964dbe8912cec8
Jasper Kennis <jasper@creativetoken.nl>
2011-11-27 22:58:36 +0100

    Created the handling of new apps, also displaying them is handled.

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,5 +1,6 @@
 class AppsController < ApplicationController
 	def index
+		@apps = App.all()
 	end
 	
 	def new
@@ -9,5 +10,26 @@ class AppsController < ApplicationController
 			format.html # new.html.erb
 			format.json { render :json => @post }
 		end
+	end
+	
+	def create
+		@app = App.new(params[:app])
+		
+		respond_to do |format|
+			if @app.save
+				format.html { redirect_to(@app,
+											:notice => 'Your app was created succesfully.') }
+				format.json { render :json => @app,
+											:status => :created, :location => @app }
+			else
+				format.html { render :action => "new" }
+				format.json { render :json => @app.errors,
+											:status => :unprocessable_entity }
+			end
+		end
+	end
+	
+	def show
+		@app = App.find(params[:id])
 	end
 end
